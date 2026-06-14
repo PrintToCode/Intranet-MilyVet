613f4fb691f9ca006d16fc2755ac305a18ddf6ec
PrintToCode <jarevalovillano@gmail.com>
2026-06-14 18:49:25 -0500

    Project basics

---
+++ Intranet_Milyvet.py
@@ -0,0 +1,172 @@
+
+print (f"Bienvenido a la intranet de MiliVet, expertos en mascotas!")
+
+while True:
+   
+       print(f"\n===MENU PRINCIPAL====")
+       print(f" 1. Inicio de sesion")
+       print(f" 2. Crear cuenta")
+       print(f" 3. Actualizacion de datos")
+       print(f" 4. Registra tu mascota!")
+       print(f" 5. Programar nueva cita")
+       print(f" 6. Reprogramar cita existente")
+
+       opcion = int(input(f"Seleccione una opcion: "))
+
+       if opcion == 1:
+          
+            print(f"\n===INICIO DE SESION===")
+              
+       
+       elif opcion == 2:
+            
+            print(f"\n===REGISTRO DE CUENTA===")
+
+            print(f"Registrese, es gratis ;D!")
+
+
+            nombres = input(f"Ingrese sus nombres: ")
+
+            nombre1,nombre2 = nombres.split()
+
+            apellidos = input(f"Ingrese su apellido: ")
+
+            apellido1,apellido2 = apellidos.split()
+
+            edad = int(input(f"Ingrese su edad: "))
+
+            departamento = input(f"Ingrese el departamento: ")
+            
+            provincia = input(f"Ingrese la provincia: ")
+
+            distrito = input(f"Ingrese el distrito: ")
+
+            direccion = input(f"Ingrese su direccion (Calle,avenida,Jiron): ")
+
+            numero_domicilio = int(input(f"Ingrese su numero de domicilio: "))
+
+            numero_dni = int(input(f"Ingrese su numero de DNI: "))
+
+            marketing = input(f"Como se entero de nosotros?: ")
+
+
+
+            print(f"Confirme su informacion antes de continuar!")
+
+            print(f"Nombres: {nombre1} {nombre2}")
+            print(f"Apellidos: {apellido1} {apellido2}")
+            print(f"Edad: {edad}")
+            print(f"Departamento: {departamento}")
+            print(f"Provincia: {provincia}")
+            print(f"Distrito: {distrito}")
+            print(f"direccion: {direccion} {numero_domicilio}")
+            print(f"Numero de DNI: {numero_dni} ")
+            print(f"")
+
+
+              
+          
+
+
+
+
+
+ 
+
+        
+      
+
+
+
+
+
+
+
+
+
+               
+
+         
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+               
+
+
+    
+
+        
+
+
+      
+
+        
+
+       
+
+     
+
+
+
+
+
+                 
+         
+
+
+
+
+                
+
+
+
+
+
+
+
+
+         
+
+     
+       
+
+
+    
+            
+    
+     
+
+     
+         
+
+    
+        
+
+
+
+    
+
+
+    
+
+
+
+
+
+
+
+
