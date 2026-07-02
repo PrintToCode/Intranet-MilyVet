2dc5ed2628f3aea3eb1cc7838ca9e0b8af49fd61
luisk9999 <>
2026-07-01 22:04:40 -0500

    Implementación de funciones validar_contrasena y nuevo_usuario

--- Intranet_Milyvet_v2.py
+++ Intranet_Milyvet_v2.py
@@ -3,6 +3,12 @@
 #Importar módulo os para crear la función "limpiar_pantalla"
 import os
 
+#Importar módulo getpass para ocultar las contraseñas ingresadas por los usuarios
+import getpass
+
+#Importar módulo re para usar regex
+import re
+
 ####CONSTRUCCION DE LA BASE DE DATOS####
 
 #Se crea una lista de usuarios para simular una tabla de BD
@@ -33,10 +39,114 @@ tabla_usuarios.append(usuario1)
 
 ####FUNCIONES####
 
-####LIMPIAR PANTALLA####
+####FUNCION LIMPIAR PANTALLA####
 def limpiar_pantalla():
     os.system("cls" if os.name == "nt" else "clear")
 ########################
+
+####FUNCION VALIDAR CONTRASEÑA####
+def validar_contrasena(contrasena):
+
+    # Se define un arreglo para guardar los erroes relacionados con la contraseña ingresada
+    errores = []
+
+    # Se evalua si la contraseña ingresada tiene una longitud menor a 20 caracteres
+    if len(contrasena) < 20:
+        #Si la contraseña no cumple con el criterio, entonces se agrega el mensaje de error respectivo al arreglo errores
+        errores.append("La contraseña debe tener una longitud mínima de 20 caracteres")
+
+    # Se evalua si la contraseña ingresada tiene por lo menos 1 caracter especial
+    if not re.search(r"[!@#$%^&*(),.?\":{}|<>_\-+=\[\]\\/;'~`]", contrasena):
+        #Si la contraseña no cumple con el criterio, entonces se agrega el mensaje de error respectivo al arreglo errores
+        errores.append("La contraseña debe tener por los menos 1 caracter especial")
+    
+    # Se evalua si la contraseña ingresada tiene por lo menos 1 número
+    if not re.search(r"[0-9]", contrasena):
+        #Si la contraseña no cumple con el criterio, entonces se agrega el mensaje de error respectivo al arreglo errores
+        errores.append("La contrseña debe tener por lo menos 1 número")
+    
+    # Se evalua si la contraseña ingresada tiene por lo menos 1 caracter en mayúscula
+    if not re.search(r"[A-Z]", contrasena):
+        #Si la contraseña no cumple con el criterio, entonces se agrega el mensaje de error respectivo al arreglo errores
+        errores.append("La contraseña debe tener por lo menos 1 caracter en mayúscula")
+    
+    # Se evalua si la contraseña ingresada tiene por lo menos 1 caracter en minúscula
+    if not re.search(r"[a-z]", contrasena):
+        #Si la contraseña no cumple con el criterio, entonces se agrega el mensaje de error respectivo al arreglo errores
+        errores.append("La contraseña debe tener por lo menos 1 caracter en minúscula")
+
+    # Si no se presentan errores de validación en la contraseña, entonces el valor de la variable contrasena_valida es true, de lo contrario es false
+    contrasena_valida = len(errores) == 0
+
+    # Se retorna la tupla contrasena_valida, errores
+    return contrasena_valida, errores
+########################
+
+####FUNCION NUEVO USUARIO####
+def nuevo_usuario():
+
+    # Se solicita al usuario ingresar su correo
+    correo = input("Ingresar correo: ")
+
+    # La validación de la contraseña se realiza mediante un bucle while
+    while True:
+
+        # Se solicita al usuario ingresar su contraseña
+        contrasena = getpass.getpass("Ingresar contraseña: ")
+
+        # Se envía la contraseña introducida por el usuario a la función validar_contrasena para evaluar su complejidad.
+        # la tupla de salida se almacena en las variables valida y lista_errores
+        valida, lista_errores = validar_contrasena(contrasena)
+
+        # Si la contraseña cumple con la complejidad
+        if valida == True:
+            # Se imprime el mensaje de éxito correspondiente
+            print("Contraseña válida ✅\n")
+            # Se sale del while
+            break
+        # Si la contraseña NO cumple con la complejidad
+        else:
+            # Se imprime el mensaje de error correspondiente
+            print("Contraseña inválida ❌. Errores encontarados:\n")
+            # Se listan los errores encontrados en la complejidad de la contraseña
+            for error in lista_errores:
+                print(f"- {error}")
+            
+            # Se solicita al usuario que presione la tecla "Enter" para volver a intentarlo ó "X" para retornar al menú principal
+            opcion = input("\nPresione Enter para volver a interntarlo ó \"X\" para volver al menú principal...\n ")
+
+        # Si el usuario presiona la tecla "X", se regreda al menú principal
+        if opcion.lower() == "x":
+            return
+
+    # Se define un arreglo para ingresar las tuplas: pregunta, respuesta del usuario
+    preguntas_respuestas = []
+
+    # Se utiliza un for para que el usuario pueda ingresar las 3 tuplas de pregunta, respuesta
+    for i in range(3):
+        # Se solicita al usuario que ingrese una pregunta de seguridad
+        pregunta = input(f"Escriba pregunta de seguridad {i + 1}: ")
+        # Se solicita al usuario que ingrese una respuesta de seguridad
+        respuesta = getpass.getpass(f"Escriba respuesta de seguridad {i + 1}: ")
+        # Se agrega la tupla pregunta, respuesta ingresada por el usuario a al arreglo preguntas_respuestas
+        preguntas_respuestas.append((pregunta, respuesta))
+    
+    # Se crea el objeto usuario_nuevo con los datos ingresados
+    usuario_nuevo = Usuario(
+        correo = correo,
+        contrasena = contrasena,
+        preguntas_respuestas = preguntas_respuestas
+    )
+
+    # Se agrega el nuevo objeto de usuario al arreglo tabla_usuarios
+    tabla_usuarios.append(usuario_nuevo)
+
+    # Se imprime mensaje de confirmación
+    print("\n¡Usuario creado de manera exitosa!\n")
+
+    # Se espera que el usuario presione enter para continuar
+    input("\nPresione Enter para continuar...")
+####################################
 
 ####FUNCION RECUPERAR CONTRASEÑA####
 def recuperar_contrasena():
@@ -78,6 +188,7 @@ def recuperar_contrasena():
 
 #Menu Principal
 while True:
+
     #Se limpia la pantalla
     limpiar_pantalla()
     
@@ -93,24 +204,29 @@ while True:
 
     #Se evalúa la opción ingresada en el menú principal
     match main_option:
+
         #INICIAR SESION
         case 1:
             #Se limpia la pantalla
             limpiar_pantalla()
 
             print("Ha seleccionado la opción 1")
+
         #NUEVO USUARIO
         case 2:
             #Se limpia la pantalla
             limpiar_pantalla()
 
-            print("Ha seleccionado la opción 2")
+            #Se ejecuta función nuevo_usuario
+            nuevo_usuario()
+
         #RECUPERAR CONTRASEÑA
         case 3:
             #Se limpia la pantalla
             limpiar_pantalla()
             #Se ejecuta función recuperar_contrasena
             recuperar_contrasena()
+
         #SALIR
         case _:
             #Se limpia la pantalla
